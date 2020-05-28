6ecc58e45afe666df8fab124438354ea45425fd8
Andes0 <41841850+Andes0@users.noreply.github.com>
2020-05-28 16:07:31 -0400

    Updates

---
+++ precache-manifest.84ac3c0e6711293bf7904ee2dd4eed24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d22069811936df97969323a319dfd0",
+    "url": "/Course-Directory/index.html"
+  },
+  {
+    "revision": "b431e91f7505d7431609",
+    "url": "/Course-Directory/static/css/main.332d6d17.chunk.css"
+  },
+  {
+    "revision": "2ee3a74f06d728bb4296",
+    "url": "/Course-Directory/static/js/2.77e80a27.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Course-Directory/static/js/2.77e80a27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b431e91f7505d7431609",
+    "url": "/Course-Directory/static/js/main.2dca5b4d.chunk.js"
+  },
+  {
+    "revision": "ed93b019b084c15cec6f",
+    "url": "/Course-Directory/static/js/runtime-main.c80aa57e.js"
+  }
+]);
