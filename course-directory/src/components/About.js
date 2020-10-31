82d1b024ad683019c4b508d2e7cb6294f1a22ca5
Andes0 <41841850+Andes0@users.noreply.github.com>
2020-10-31 18:16:28 -0400

    j

--- course-directory/src/components/About.js
+++ course-directory/src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = (props) => (
   <div className="main-content">
     <h2>{ props.title }</h2>
-    <p>This React.js based website consists of a directory of teachers at CSN's science department as well as </p>
+    <p>This React.js based website consists of a directory of teachers at CSN's science department as well as a general course offerings page.</p>
   </div>
 );
 
