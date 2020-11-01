a50c48b86f00447390dc0f198131053b046becec
Andes0 <41841850+Andes0@users.noreply.github.com>
2020-11-01 11:34:06 -0500

    fixing link on homepage

--- course-directory/src/components/Home.js
+++ course-directory/src/components/Home.js
@@ -11,7 +11,7 @@ class Home extends Component {
     return (
       <div className="main-content home">
         <h2>Front End Course Directory</h2>
-        <p>This directory is a practice project for a staff page for <a href="https://www.communityschoolnaples.org/index.cfm/">Community School of Naples</a>, specifically for the Science Department</p>
+        <p>This directory is a practice project for a staff page for <a href="https://www.communityschoolnaples.org/">Community School of Naples</a>, specifically for the Science Department</p>
         <p>This page uses <a href = "https://reactjs.org/">Facebook's React.js Javascript Library</a> to create a sample course directory for CSN's science department, including teachers and courses offered.</p>
         <p>Images used are either sourced from the internet or the school website. Quality of images may suffer due to school website's image compression</p>
         <hr />
